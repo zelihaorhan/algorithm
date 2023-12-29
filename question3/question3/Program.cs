612977d926c22933dc2728fb0ee3ce8fe278d73c
zelihaorhan <zeliha.orhan2399@gmail.com>
2023-12-30 01:35:41 +0300

    Find the sum of the cubes of numbers from 1 to 100

---
+++ question3/question3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code to find the sum of the cubes of numbers from 1 to 100
+            double sum = 0;
+            for(int i=1;i<100;i++)
+            {
+                sum +=(Math.Pow(i, 3));
+            }
+            Console.WriteLine("Sum:" + sum);
+
+            Console.ReadLine(); 
+        }
+    }
+}
