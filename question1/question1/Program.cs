60ca992fcbe0a4a649ab94027cb63cfac9a3b6a5
zelihaorhan <zeliha.orhan2399@gmail.com>
2023-12-30 01:24:19 +0300

    Add two numbers

---
+++ question1/question1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code for the program that returns the sum of two numbers
+
+            Console.Write("Enter first number:");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sum: " + (n1 + n2));
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
