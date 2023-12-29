57a3eaee34729112731cef74c9650459d211cd31
zelihaorhan <zeliha.orhan2399@gmail.com>
2023-12-30 01:43:46 +0300

    Calculate the age

---
+++ question4/question4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculate the age of the person whose date of birth is entered
+
+            Console.Write("Enter the birth year:");
+            int bYear=Convert.ToInt32(Console.ReadLine());
+
+            int currentYear=DateTime.Now.Year;
+
+            Console.Write("Age: " + (currentYear - bYear));
+
+            Console.ReadLine(); 
+        }
+    }
+}
