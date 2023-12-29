898bbc8dc900954321317585670aeed8a3b3fd15
zelihaorhan <zeliha.orhan2399@gmail.com>
2023-12-30 01:48:01 +0300

    Calculate the factorial

---
+++ question5/question5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculate the factorial of the number entered by the user
+            Console.Write("Enter the number:");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int fac = 1;
+            for(int i=1;i<=n;i++)
+            {
+                fac *= i;
+            }
+            Console.Write("Result:" + fac);
+
+            Console.ReadLine(); 
+        }
+    }
+}
