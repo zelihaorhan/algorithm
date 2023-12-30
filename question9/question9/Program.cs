34e523341a64c05d492d5a940c1e7321fbd325a6
zelihaorhan <zeliha.orhan2399@gmail.com>
2023-12-30 15:20:52 +0300

    Find digit number

---
+++ question9/question9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code that finds how many digits the entered number has
+            Console.Write("Enter the number:");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int digit = 1;
+            while(n>9)
+            {
+                n/=10; 
+                digit++;
+            }
+            Console.WriteLine("Digit number: " + digit);
+
+            Console.ReadLine();
+        }
+    }
+}
