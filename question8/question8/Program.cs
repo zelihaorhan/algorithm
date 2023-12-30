b48b4f0c3852f81b32cd2133f8f12226284b21d1
zelihaorhan <zeliha.orhan2399@gmail.com>
2023-12-30 15:15:50 +0300

    Calculate the mode

---
+++ question8/question8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code that calculates the mode of the entered number according to the desired number.
+
+            Console.WriteLine("Enter number:");
+            int number=Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine("Enter mode:");
+            int mode=Convert.ToInt32(Console.ReadLine());
+
+            int result = number-(number/mode)*mode;
+
+            
+
+            Console.WriteLine("Result:" + result);
+
+            Console.ReadLine();
+        }
+    }
+}
