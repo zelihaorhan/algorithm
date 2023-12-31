2d2d748578b8dca2c8ea7e3a0696714c9f7e2d58
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-01 00:25:40 +0300

    Find perfect square numbers

---
+++ question12/question12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code for a program that prints perfect square numbers between 10 and 1000.
+
+            for(int i = 1; i <=31; i++)
+            {
+                int square=i*i;
+                if (square >= 10 && square <= 1000)
+                {
+                    Console.WriteLine(square);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
