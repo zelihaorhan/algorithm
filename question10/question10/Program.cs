67e2a55053789ebf3aa72f0064fd6b584f1becbb
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-01 00:03:04 +0300

    Find whether the sum of the cubes of the digits of a 3-digit number is equal to itself

---
+++ question10/question10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Code to find whether the sum of the cubes of the digits of a 3-digit number is equal to itself
+
+            Console.WriteLine("Enter the number:");
+            int number=Convert.ToInt32(Console.ReadLine());
+            int fDigit = number / 100;
+            int sDigit = (number / 10)%10;
+            int tDigit = number % 10;
+            if (fDigit * fDigit * fDigit + sDigit * sDigit * sDigit + tDigit * tDigit * tDigit == number)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
