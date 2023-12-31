1fc7c5b55c620767820772b2bd437b3c74952b7d
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-01 00:49:43 +0300

    Create multiplication table

---
+++ question14/question14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code for the multiplication table
+            for(int i=1;i<=10;i++)
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine(i + "*" + j + "=" + i * j);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
