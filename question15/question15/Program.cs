9e17e1a506b2857a4cf86e5356b992284c44dfad
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-02 13:28:20 +0300

    Find 'ıs it a power of 5'

---
+++ question15/question15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code for the program that finds whether the entered number is a power of 5 or not
+            Console.WriteLine("Enter the number:");
+            int number=Convert.ToInt32(Console.ReadLine());
+
+            if(IsFivePower(number))
+            {
+                Console.WriteLine("Number is power of 5");
+            }
+            else
+            {
+                
+                    Console.WriteLine("Number is not power of 5");
+                
+            }
+            Console.ReadLine(); 
+
+        }
+        static bool IsFivePower(int num)
+        {
+            while (num > 1)
+            {
+                if(num%5!=0)
+                {
+                    return false;
+                }
+                num /= 5;
+            }
+            return num == 1;
+        }
+    }
+}
