f90d8310c50fe4b6702e2886979b089f259ceb39
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-03 22:34:42 +0300

    Find friendly numbers

---
+++ question16/question16/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //If x and y are positive integers, if the sum of the factors of x is equal to the number y and at the same time the sum of the factors of y is equal to the number x, these numbers are friendly numbers. Accordingly, write the code of the program that finds whether the two entered numbers are friends or not.
+            Console.WriteLine("Please enter the X:");
+            int x=Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine("Please enter the Y:");
+            int y=Convert.ToInt32(Console.ReadLine());
+
+            int xSum = 0;
+            int ySum = 0;
+
+            for(int i=1; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    xSum += i;
+                }
+            }
+            for(int i=1;i<y; i++)
+            {
+                if(y%i == 0)
+                {
+                    ySum += i;
+                }
+            }
+
+            if (xSum == y && ySum == x)
+            {
+                Console.WriteLine("X and Y are friendly numbers :)");
+            }
+            else
+            {
+                Console.WriteLine("X and Y are not friendly numbers :(");
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ question16/question16/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("question16")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("question16")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("2adaa9da-2d4b-4167-97c7-09b7475413b6")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
