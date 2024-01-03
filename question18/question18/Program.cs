c3c21df15b60a9ad896732481ffeda30179deb23
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-03 22:57:10 +0300

    Find positive,negative or zero

---
+++ question18/question18/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code that finds whether a number entered from the keyboard is negative, positive or 0
+
+            Console.WriteLine("Enter the number:");
+            int n=Convert.ToInt32(Console.ReadLine());
+            if(n<0)
+            {
+                Console.WriteLine("Number is negative");
+            }
+            else if(n>0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else
+            {
+                Console.WriteLine("Number is zero");
+            }
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
