6bcffd3b7dcb56313bd6bd1aea8ea6c3ed4ae055
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-03 22:51:32 +0300

    Write the first terms of the Fibonacci series

---
+++ question17/question17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code that prints the first 10 terms of the Fibonacci series to the screen
+
+            int first = 1;
+            int second = 1;
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            for (int i=0;i<8;i++)
+            {
+                
+                int temp=first+second;
+                first = second;
+                second = temp;
+                Console.WriteLine(temp);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
