3006a21748020e717f6d364b7683da34d10881b2
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-04 23:03:19 +0300

    Find perfect number

---
+++ question20/question20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code to find out whether the sum of even numbers between 1-100 is a perfect number.
+            int sum = 0;
+            int sum2 = 0;
+            for(int i=1;i<100;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            for(int i = 1; i < sum; i++)
+            {
+                if(sum%i== 0)
+                {
+                    sum2 += i;
+                }
+            }
+            if(sum2==sum)
+            {
+                Console.WriteLine("The sum of even numbers between 1-100 is a perfect number.");
+
+            }
+            else
+            {
+                Console.WriteLine("The sum of even numbers between 1-100 is not a perfect number.");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
