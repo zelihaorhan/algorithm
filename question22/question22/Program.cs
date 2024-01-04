dbb02036c90d3053e9c23fd9922b51d4a92a0c58
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-04 23:19:36 +0300

    Decide enter the number is abundant or deficient

---
+++ question22/question22/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code that finds whether the entered number is an abundant number or a deficient number.
+            Console.WriteLine("Enter the number:");
+            int number=Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            for(int i=1;i<number;i++)
+            {
+                if (number % i == 0)
+                {
+                    result += i;
+                }
+            }
+            if (number > result)
+            {
+                Console.WriteLine("Number is abundant");
+            }
+            else
+            {
+                Console.WriteLine("Number is deficient");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
