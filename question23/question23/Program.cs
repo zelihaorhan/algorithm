b8bb31d429a271831c1d51a57fc395033ead5f46
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-01-04 23:22:58 +0300

    Find the sum of from 1 to 500

---
+++ question23/question23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write the code for the program that finds the sum of integers from 1 to 500
+            int sum = 0;
+            for(int i=1;i<500;i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
